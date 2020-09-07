ce55a87c3322573d64efe39fb57a478f4f3562ce
Michael A Tomcal <michael.a.tomcal@aexp.com>
2020-09-07 15:48:23 -0700

    chore(codesandbox): add

---
+++ examples/next-js/fetchye-ssr/pages/index.js
@@ -0,0 +1,46 @@
+import { Layout } from "../components/Layout";
+import SimpleCache from "fetchye/lib/cache/SimpleCache";
+import { useFetchye, makeServerFetchye } from "fetchye";
+
+const cache = SimpleCache();
+
+// Codesandbox takes a second to get Next.JS started...
+export default function IndexPage({ initialPerson }) {
+  const { isLoading, data } = useFetchye(
+    "https://bulyq.sse.codesandbox.io/profile",
+    {
+      initialData: initialPerson
+    }
+  );
+  return (
+    <Layout>
+      <p>
+        {isLoading && (
+          <span aria-label="loading" role="img">
+            ⏳
+          </span>
+        )}
+        {!isLoading && data?.body.name}
+      </p>
+   </Layout>
+  );
+}
+
+const fetchye = makeServerFetchye({
+  cache,
+  fetchClient: fetch
+});
+
+export async function getServerSideProps() {
+  try {
+    const res = await fetchye("https://bulyq.sse.codesandbox.io/profile");
+    return {
+      props: {
+        initialPerson: res
+      }
+    };
+  } catch (error) {
+    console.error(error.message);
+    return {};
+  }
+}
