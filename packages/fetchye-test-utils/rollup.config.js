ee987df4ef22ff74c010406a0a34466fd1513a85
Jonathan Adshead <jonathan.adshead@aexp.com>
2020-11-03 11:36:04 -0700

    chore(rollup-config): update peer deps

--- packages/fetchye-test-utils/rollup.config.js
+++ packages/fetchye-test-utils/rollup.config.js
@@ -21,36 +21,15 @@ import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const inputSrc = 'src/index.js';
-
+const external = ['fetchye-core'];
 export default [
-  {
-    input: inputSrc,
-    output: {
-      name: 'fetchye-test-utils',
-      globals: {
-        react: 'React',
-      },
-      file: pkg.browser,
-      format: 'umd',
-    },
-    plugins: [
-      nodeResolve({ browser: true }),
-      commonjs(),
-      babel({
-        babelHelpers: 'runtime',
-        exclude: 'node_modules/**',
-        presets: [['amex', { 'preset-env': { modules: false } }]],
-        plugins: [['@babel/plugin-transform-runtime', { useESModules: true }]],
-      }),
-      terser(),
-    ],
-  },
   {
     input: inputSrc,
     output: {
       file: pkg.main,
       format: 'cjs',
     },
+    external,
     plugins: [
       nodeResolve(),
       commonjs(),
@@ -69,6 +48,7 @@ export default [
       file: pkg.module,
       format: 'es',
     },
+    external,
     plugins: [
       nodeResolve(),
       commonjs(),
